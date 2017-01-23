440f05909190f0c56b1c6b4754480d9f972df339
Tom Lillis <thli2089@colorado.edu>
2017-01-23 14:16:19 -0700

    added confusion matrix

--- TomSandBox/test_nn/testNN.py
+++ TomSandBox/test_nn/testNN.py
@@ -7,6 +7,8 @@ from databaseProxy import DatabaseProxy
 import tensorflow as tf
 import numpy as np
 
+from sklearn.metrics import confusion_matrix
+
 # Data sets
 
 print("Loading Dataset...")
@@ -24,13 +26,13 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 print("Building Classifier...")
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                            hidden_units=[10, 20, 10],
+                                            hidden_units=[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6],
                                             n_classes=6)
 print("Finished Building Classifier.")
 
 # Fit model.
 print("Fitting Model...")
-classifier.fit(x=trainingData[:len(trainingData)/100], y=trainingLabels[:len(trainingData)/100], steps=100)
+classifier.fit(x=trainingData[:len(trainingData)/100], y=trainingLabels[:len(trainingData)/100], steps=10)
 print("Finished Fitting Model.")
 
 # Evaluate accuracy.
@@ -38,4 +40,8 @@ print("Evaluating Accuracy...")
 accuracy_score = classifier.evaluate(x=testData, y=testLabels)["accuracy"]
 print("Finished Evaluating Accuracy.")
 print('Accuracy: {0:f}'.format(accuracy_score))
-
+print("Building Confusion Matrix...")
+predictions = list(classifier.predict(testData, as_iterable=True))
+confusion = confusion_matrix(testLabels,predictions)
+print("Confusion matrix:")
+print(confusion)
