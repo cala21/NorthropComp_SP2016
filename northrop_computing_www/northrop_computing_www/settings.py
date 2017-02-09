e49689a4ccfd9f27b4c3126e92a54c0d3f6790a7
Ryan Riley <ryan.b.riley@colorado.edu>
2017-02-08 22:23:33 -0700

    adding bootstrap to web app

--- northrop_computing_www/northrop_computing_www/settings.py
+++ northrop_computing_www/northrop_computing_www/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'northrop_computing_www.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,5 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+print 'static root: ' + STATIC_ROOT
 STATIC_URL = '/static/'
--- northrop_computing_www/northrop_computing_www/views.py
+++ northrop_computing_www/northrop_computing_www/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    html = "Future Site of Northrop Computing - Index under construction" + '<br><br><img src="../static/northrop_computing_www/northrop.jpg"><br><br><a href="/classify/">Image Classifier</a>'
-    return HttpResponse(html)
+    return render(request,'northrop_computing_www/index.html',{ })
