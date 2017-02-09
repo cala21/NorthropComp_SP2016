a2943a6696088cffd3a0291f03188d5c6034b43b
Ryan Riley <ryan.b.riley@colorado.edu>
2017-02-09 00:03:30 -0700

    updating demo links

--- northrop_computing_www/classify/views.py
+++ northrop_computing_www/classify/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Future Site of Northrop Computing Web-Based Classifier")
+    return render(request,'classify/index.html',{ })
