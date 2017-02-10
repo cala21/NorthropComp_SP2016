fdfaa48a6e2a25ef4980d1fbef7659d1196631da
Ryan Riley <ryan.b.riley@colorado.edu>
2017-02-10 11:55:28 -0700

    adding image display

--- northrop_computing_www/classify/urls.py
+++ northrop_computing_www/classify/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='classifier'),
+    url(r'^demo$', views.demo, name='demo'),
 ]
--- northrop_computing_www/classify/views.py
+++ northrop_computing_www/classify/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from classify.models import imageUpload
 from .forms import imageForm
 # Create your views here.
 
@@ -9,11 +10,17 @@ def index(request):
         form = imageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/classify/demo')
     else:
         form = imageForm()
     return render(request, 'classify/index.html', {
         'form': form
     })
+
+def demo(request):
+    image_data = imageUpload.objects.all().latest('uploaded_at').image#latest('uploaded_at')
+    #print image_data
+    #html = '<img src="/media/documents/krHJs8F.jpg">'
+    return HttpResponse(image_data, content_type="image/jpg")
 
     #return render(request,'classify/index.html',{ })
